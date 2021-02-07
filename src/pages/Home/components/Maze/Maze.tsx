9467d6c295efa31f6c8475535243ab98c5d567a3
细胞 <549024307@qq.com>
2021-02-07 17:59:16 +0800

    no message

--- src/pages/Home/components/Maze/Maze.tsx
+++ src/pages/Home/components/Maze/Maze.tsx
@@ -12,21 +12,21 @@ class Maze {
   columns: number
   cells: number
   rows: number
-  linkedMap: Object
+  linkedMap: Map<number,Array<any>>
   unionSet: UnionSet
   constructor(columns: number, rows: number) {
     this.columns = columns
     this.rows = rows
     this.cells = columns * rows
     //  存放联通的格子，{1:[2,11]}表示第1个和2,11联通
-    this.linkedMap = {}
+    this.linkedMap = new Map()
     //  初始化连通集
     this.unionSet = new UnionSet(this.cells)
   }
   //  创建
   generate(): any {
     //  判断第一个是否与最后一个格子连通，如果不是的话，则每次随机选取两个相邻的格子，如果它们不在同一个连通集，则把它们连通一下，同时记录一下拆掉的墙到linkedMap里面
-    while (!this.firstLastLinked()) {
+    while (!this.linkedToFirstCell()) {
       let cellPairs = this.pickRandomCellPairs()
       if (!this.unionSet.sameSet(cellPairs[0], cellPairs[1])) {
         this.unionSet.union(cellPairs[0], cellPairs[1])
@@ -35,19 +35,59 @@ class Maze {
     }
   }
   // 第一个和最后一个格子是否联通
-  firstLastLinked(): Boolean {
-    return this.unionSet.sameSet(0, this.cells)
+  linkedToFirstCell(): Boolean {
+    for(let i=1;i<this.cells;i++){
+      if(!this.unionSet.sameSet(0,i)) return false
+    }
+    return true
   }
   //  随机选取相邻的两个格子
   pickRandomCellPairs(): Array<any> {
     let randomDoubleCell: Array<any> = []
     let randomCell:any=Math.ceil(Math.random()*this.cells)
-    console.log(randomCell);
-    
+    // 获取四周的格子
+    let neiborCells:Array<any>=[]
+    let row=Math.floor(randomCell/this.columns)
+    let column=randomCell%this.rows
+    if(row!==0){
+      neiborCells.push(randomCell-this.columns)
+    }
+    if(row!==this.rows-1){
+      neiborCells.push(randomCell+this.columns)
+    }
+
+    if(column!==0){
+      neiborCells.push(randomCell-1)
+    }
+
+    if(column!==this.columns-1){
+      neiborCells.push(randomCell+1)
+    }
+
+    // 相邻的两个格子
+    randomDoubleCell[0]=randomCell
+    randomDoubleCell[1]=neiborCells[~~(Math.random()*neiborCells.length)]
     return randomDoubleCell
   }
   //  记录两个联通的格子之间的墙
-  addLinkedMap(cell1: any, cell2: any): any {
+  addLinkedMap(cell1: any, cell2: any): void {
+
+    let mapCell1:any=this.linkedMap.get(cell1)
+    let mapCell2:any=this.linkedMap.get(cell2)
+    if(!mapCell1){
+      this.linkedMap.set(cell1,[])
+    }
+    if(!mapCell2){
+      this.linkedMap.set(cell2,[])
+    }
+
+    if(mapCell1?.indexOf(cell2)==-1){
+      mapCell1.push(cell2)
+    }
+
+    if(mapCell2?.indexOf(cell1)==-1){
+      mapCell2.push(cell1)
+    }
 
   }
 }
--- src/pages/Home/components/Maze/MazeCanvas.tsx
+++ src/pages/Home/components/Maze/MazeCanvas.tsx
@@ -7,26 +7,75 @@
 import React from 'react'
 import Maze from './Maze'
 import './MazeCss.scss'
-interface Props{
+interface Props {
 
 }
-interface State{
+interface State {
 
 }
 
-class MazeCanvas extends React.Component<Props,State>{
-  Maze:Maze
-  canvas:any
-  constructor(Props:any){
+class MazeCanvas extends React.Component<Props, State>{
+  Maze: Maze
+  canvas: any
+  constructor(Props: any) {
     super(Props)
-    this.canvas=document.getElementById('maze')
-    this.Maze=new Maze(4,4)
+    this.canvas = React.createRef();
+    this.Maze = new Maze(20,20)
+    this.Maze.generate()
+    console.log(this.Maze);
   }
 
-  render():any{
+  componentDidMount() {
+    this.canvasMap()
+  }
+
+  canvasMap() {
+    let canvas = this.canvas.current
+    let ctx = canvas.getContext('2d')
+    if (ctx) {
+      this.draw(canvas, ctx)
+    }
+  }
+  draw(canvas: any, ctx: any) {
+    let linkedMap = this.Maze.linkedMap
+    console.log(linkedMap);
+    
+    let cellWidth = canvas.width / this.Maze.columns
+    let cellHeight = canvas.height / this.Maze.rows
+    // translate 0.5像素，避免模糊
+    // ctx.translate(0.5, 0.5)
+
+    for (let i = 0; i < this.Maze.cells; i++) {
+      let row:number = i / this.Maze.columns >> 0
+      let column :number= i % this.Maze.columns
+      let mapLinkedCell:any=linkedMap.get(i)
+      if(column!==this.Maze.columns-1 && (!mapLinkedCell || mapLinkedCell?.indexOf(i+1)!=-1)){
+        ctx.moveTo((column+1)*cellWidth,row*cellHeight)
+        ctx.lineTo((column+1)*cellWidth,(row+1)*cellHeight)
+      }
+      if(row!==this.Maze.rows-1 && (!mapLinkedCell || mapLinkedCell?.indexOf(i+this.Maze.columns)!=-1)){
+        ctx.moveTo(column*cellWidth,(row+1)*cellHeight)
+        ctx.lineTo((column+1)*cellWidth,(row+1)*cellHeight)
+      }
+    }
+    this.drawBorder(ctx,canvas.width,canvas.height)
+    ctx.stroke()
+  }
+  drawBorder(ctx:any,width:number,height:number){
+    ctx.moveTo(0,0)
+    ctx.lineTo(width,0)
+    ctx.moveTo(width,0)
+    ctx.lineTo( width, height)
+    ctx.moveTo( width-(width/ this.Maze.columns), height)
+    ctx.lineTo(0,height)
+    ctx.moveTo(0,height)
+    ctx.lineTo(0,height/ this.Maze.rows)
+  }
+
+  render(): any {
     return (
       <div>
-        <canvas id='maze' width='800' height='600' className="maze__canvas"></canvas>
+        <canvas ref={this.canvas} width='800' height='600' className="maze__canvas"></canvas>
       </div>
     )
   }
